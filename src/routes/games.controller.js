b5d19a9752bd38abd426951989b35297cfb4c875
ljravelli23 <ljravelli.23@est.ucab.edu.ve>
2026-05-02 19:43:26 -0400

    creación en el servicio

--- src/routes/games.controller.js
+++ src/routes/games.controller.js
@@ -3,7 +3,6 @@ import gamesService from './games.service.js';
 const gamesController = {};
 
 gamesController.getAll = (req, res) => {
-    res.status(200).json([]);
     const games = gamesService.findAll();
     res.status(200).json(games);
 };
@@ -13,7 +12,6 @@ gamesController.getOne = (req, res) => {
 };
 
 gamesController.create = (req, res) => {
-    res.status(201).json({});
     const newGame = gamesService.create(req.body);
     res.status(201).json({
         message: "Juego registrado exitosamente",
@@ -26,14 +24,6 @@ gamesController.update = (req, res) => {
 };
 
 gamesController.delete = (req, res) => {
-    const deletedGame = gamesService.delete(req.params.id);
-    if (!deletedGame) {
-        return res.status(404).json({ message: "No se pudo eliminar, juego no encontrado" });
-    }
-    res.status(200).json({
-        message: "Juego retirado del catálogo",
-        game: deletedGame
-    });
     res.status(200).json({});
 };
 
--- src/routes/games.service.js
+++ src/routes/games.service.js
@@ -21,25 +21,4 @@ gamesService.create = (data) => {
     return newGame;
 };
 
-gamesService.findById = (id) => {
-    return games.find(g => g.id === parseInt(id));
-};
-
-gamesService.update = (id, data) => {
-    const index = games.findIndex(g => g.id === parseInt(id));
-    if (index !== -1) {
-        games[index] = { ...games[index], ...data, id: parseInt(id) };
-        return games[index];
-    }
-    return null;
-};
-
-gamesService.delete = (id) => {
-    const index = games.findIndex(g => g.id === parseInt(id));
-    if (index !== -1) {
-        return games.splice(index, 1)[0];
-    }
-    return null;
-};
-
 export default gamesService;
