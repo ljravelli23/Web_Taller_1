2be7389204236fe72e4af62329514bc678fd0069
ljravelli23 <ljravelli.23@est.ucab.edu.ve>
2026-05-02 20:27:01 -0400

    games routes

---
+++ src/modules/Games/games.routes.js
@@ -0,0 +1,24 @@
+import { Router } from 'express';
+import gamesController from './games.controller.js';
+
+const gamesRouter = Router();
+
+gamesRouter.get('/', gamesController.getAll);
+gamesRouter.get('/:id', gamesController.getOne);
+gamesRouter.post('/', gamesController.create);
+gamesRouter.put('/:id', gamesController.update);
+gamesRouter.delete('/:id', gamesController.delete);
+import { Router } from 'express';
+import gamesController from './games.controller.js';
+
+const gamesRouter = Router();
+
+gamesRouter.get('/', gamesController.getAll);
+gamesRouter.get('/:id', gamesController.getOne);
+gamesRouter.post('/', gamesController.create);
+gamesRouter.put('/:id', gamesController.update);
+gamesRouter.delete('/:id', gamesController.delete);
+
+export default gamesRouter;
+
+export default gamesRouter;
