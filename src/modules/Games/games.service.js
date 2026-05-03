310f501861cf66d775501f77306e9e2c160f6b96
ljravelli23 <ljravelli.23@est.ucab.edu.ve>
2026-05-02 20:18:16 -0400

    Correcion de carpetas duplicada, titulo de archivos ahora todos en minúscula, delete ahora si existe

--- src/modules/Games/games.service.js
+++ src/modules/Games/games.service.js
@@ -7,11 +7,37 @@ gamesService.findAll = () => {
     return games;
 };
 
+gamesService.findById = (id) => {
+    return games.find(g => g.id === parseInt(id));
+};
+
+gamesService.create = (data) => {
+    const newGame = {
+        id: idCounter++,
+        name: data.name,
+        minPlayers: data.minPlayers,
+        maxPlayers: data.maxPlayers,
+        avgDuration: data.avgDuration,
+        acquisitionDate: data.acquisitionDate,
+        status: data.status
+    };
+    games.push(newGame);
+    return newGame;
+};
+
 gamesService.update = (id, data) => {
     const index = games.findIndex(g => g.id === parseInt(id));
     if (index !== -1) {
         games[index] = { ...games[index], ...data, id: parseInt(id) };
         return games[index];
+    }
+    return null;
+};
+
+gamesService.delete = (id) => {
+    const index = games.findIndex(g => g.id === parseInt(id));
+    if (index !== -1) {
+        return games.splice(index, 1)[0];
     }
     return null;
 };
