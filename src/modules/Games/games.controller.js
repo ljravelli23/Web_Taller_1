6f454fd29e1b3077822b29738ecab3ac7b6680cb
ljravelli23 <ljravelli.23@est.ucab.edu.ve>
2026-05-02 20:38:17 -0400

    mensaje de exito o no encontrado en el controlador

--- src/modules/Games/games.controller.js
+++ src/modules/Games/games.controller.js
@@ -8,11 +8,19 @@ gamesController.getAll = (req, res) => {
 };
 
 gamesController.getOne = (req, res) => {
-    res.status(200).json({});
+    const game = gamesService.findById(req.params.id);
+    if (!game) {
+        return res.status(404).json({ message: "Juego no encontrado" });
+    }
+    res.status(200).json(game);
 };
 
 gamesController.create = (req, res) => {
-    res.status(201).json({});
+    const newGame = gamesService.create(req.body);
+    res.status(201).json({
+        message: "Juego registrado exitosamente",
+        game: newGame
+    });
 };
 
 gamesController.update = (req, res) => {
@@ -27,7 +35,14 @@ gamesController.update = (req, res) => {
 };
 
 gamesController.delete = (req, res) => {
-    res.status(200).json({});
+    const deletedGame = gamesService.delete(req.params.id);
+    if (!deletedGame) {
+        return res.status(404).json({ message: "No se pudo eliminar, juego no encontrado" });
+    }
+    res.status(200).json({
+        message: "Juego retirado del catálogo",
+        game: deletedGame
+    });
 };
 
 export default gamesController;
